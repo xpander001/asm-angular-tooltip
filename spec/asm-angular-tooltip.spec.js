1ca75a3f97dcd991525d9e38a593af38ebca793e
Ander Suarez Martinez <me@andersuarez.com>
2016-05-18 12:41:17 +0200

    Add tooltip show and cancel hide on tooltip hover (this is the main reason to develop my own tooltip)

--- spec/asm-angular-tooltip.spec.js
+++ spec/asm-angular-tooltip.spec.js
@@ -2,8 +2,8 @@
 
 describe('Asm angular tooltip', function() {
 
-  var compile;
   var scope;
+  var elementBody;
   var element;
   var elementScope;
   var timeout;
@@ -12,13 +12,12 @@ describe('Asm angular tooltip', function() {
     module('asmAngularTooltip');
 
     inject(function($compile, $rootScope, $timeout) {
-      compile = $compile;
+      elementBody = angular.element('<div><p asm-angular-tooltip="tooltip text">Selector Text</p></div>');
       scope = $rootScope;
       timeout = $timeout;
-
-      element = angular.element('<p asm-angular-tooltip="tooltip text">Selector Text</p>');
-      compile(element)(scope);
+      $compile(elementBody)(scope);
       scope.$digest();
+      element = elementBody.find('p');
       elementScope = element.scope();
     });
 
@@ -26,21 +25,47 @@ describe('Asm angular tooltip', function() {
 
   it('should be closed', function() {
     expect(elementScope.isOpen).toBe(false);
+    expect(elementBody.children().length).toBe(1);
   });
 
   it('should open on mouseenter', function() {
     element.triggerHandler('mouseenter');
     expect(scope.isOpen).toBe(true);
+    // Tooltip added
+    expect(elementBody.children().length).toBe(2);
   });
 
   it('should close on mouseleave after 1 second', function() {
     element.triggerHandler('mouseenter');
     element.triggerHandler('mouseleave');
     expect(scope.isOpen).toBe(true);
+    expect(elementBody.children().length).toBe(2);
     timeout.flush(500);
     expect(scope.isOpen).toBe(true);
+    expect(elementBody.children().length).toBe(2);
     timeout.flush(500);
     expect(scope.isOpen).toBe(false);
+    expect(elementBody.children().length).toBe(1);
+  });
+
+  it('should stay open if tooltip is hovered', function() {
+    element.triggerHandler('mouseenter');
+    element.triggerHandler('mouseleave');
+    expect(scope.isOpen).toBe(true);
+    var tooltip = angular.element(elementBody.children()[1]);
+    tooltip.triggerHandler('mouseenter');
+    timeout.flush(1200);
+    expect(scope.isOpen).toBe(true);
+    tooltip.triggerHandler('mouseleave');
+    timeout.flush(500);
+    expect(scope.isOpen).toBe(true);
+    timeout.flush(600);
+    expect(scope.isOpen).toBe(false);
+  });
+
+  it('should have text property', function() {
+    expect(scope.text).toBeDefined();
+    expect(scope.text).toEqual('tooltip text');
   });
 
 });
--- src/directive.js
+++ src/directive.js
@@ -7,7 +7,7 @@
     .directive('asmAngularTooltip', asmAngularTooltip);
 
   /* @ngInject */
-  function asmAngularTooltip($timeout) {
+  function asmAngularTooltip($timeout, $compile) {
 
     var directive = {
       restrict: 'A',
@@ -17,23 +17,65 @@
 
     var CLOSE_DELAY = 1000;
 
+    var tooltipTemplate = '<div class="asm-tooltip asm-tooltip-up">{{text}}<div class="asm-tooltip-arrow"></div></div>';
+
     return directive;
 
     function link(scope, elem, attrs) {
-      scope.isOpen = false;
-      bindEvents();
+
+      var tooltip = $compile(tooltipTemplate)(scope);
+      var visibleClassName = 'asm-tooltip-visible';
+      var closingTimeout;
+
+      init();
+
       /* Private functions */
+      function init() {
+        scope.isOpen = false;
+        scope.text = attrs.asmAngularTooltip;
+
+        bindEvents();
+      }
 
       function bindEvents() {
         elem.bind('mouseenter', function() {
+          $timeout.cancel(closingTimeout);
           scope.isOpen = true;
+          show();
         });
 
         elem.bind('mouseleave', function() {
-          $timeout(function() {
+          closingTimeout = $timeout(function() {
             scope.isOpen = false;
+            hide();
           }, CLOSE_DELAY);
         });
+
+        tooltip.bind('mouseenter', function() {
+          $timeout.cancel(closingTimeout);
+          scope.isOpen = true;
+          show();
+        });
+
+        tooltip.bind('mouseleave', function() {
+          closingTimeout = $timeout(function() {
+            scope.isOpen = false;
+            hide();
+          }, CLOSE_DELAY);
+        });
+      }
+
+      function show() {
+        tooltip.addClass(visibleClassName);
+        elem.after(tooltip);
+      }
+
+      function hide() {
+        tooltip.remove();
+      }
+
+      function positionTooltip() {
+        var offset;
       }
     }
   }
